db250330f465c0f4749200105fc00d2fa306dc88
morawi-cg <mo.rawi@capgemini.com>
2017-03-15 16:52:18 +0000

    I have added a mended class tha is suppose to give a return

--- dpl_api/wercker/models.py
+++ dpl_api/wercker/models.py
@@ -4,18 +4,28 @@ import requests
 import json
 from json2html import *
 
-#os.system("../../../../../Auth/authFile.py")
+os.system("/home/devops/Documents/Python/Auth/authFile.py")
 url = 'https://app.wercker.com/api/v3/applications/wercker/docs'
 headers = {'Authorization':'TokenName Token'}
 
-class wercker(models.Model):
+#class wercker(models.Model):
 #“””Model for wercker
 #“””
-    TokenName = models.CharField(max_length=30,help_text="name of the wercker token")
-    Token = models.CharField(max_length=30,help_text="This is the wercker token")
+#    TokenName = models.CharField(max_length=30,help_text="name of the wercker token")
+#    Token = models.CharField(max_length=30,help_text="This is the wercker token")
     #url = models.CharField(default=None,max_length=255,help_text="BuildTool API url")
-    r = requests.get(url, headers=headers, verify=True)
-    inp = r.json()
-    print(json2html.convert(json = inp))
-    def _unicode_(self):
-         return self.choice_text
+#    r = requests.get(url, headers=headers, verify=True)
+#    inp = r.json()
+#    print(json2html.convert(json = inp))
+#    def _unicode_(self):
+#         return self.choice_text
+
+class wercker(models.Model):
+      def __init__(self,TokenName,Token):
+          self.TN = TokenName
+          self.T  = Token
+          r = requests.get(url, headers=headers, verify=True)
+          inp = r.json()
+          ScreenValues = json2html.convert(json = inp)
+          return ScreenValues
+
