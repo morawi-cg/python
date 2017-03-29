1f81a2fd686fc4e6e2f5af224ca83506f69a3fc6
morawi-cg <mo.rawi@capgemini.com>
2017-03-29 11:54:37 +0100

    Building the first page reply from within Django

--- dpl01/wercker/views.py
+++ dpl01/wercker/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from json2html import *
+import requests, json 
+
 # Create your views here.
-def detail(request, question_id):
-    return HttpResponse("wercker Account Credentials %s." % question_id)
+#def detail(request, question_id):
+def detail(TokeName,Token):
+    url = 'https://app.wercker.com/api/v3/applications/wercker/docs'
+    headers = {'Authorization':'TokenName Token'}
+    r = requests.get(url, headers=headers, verify=True)
+    inp = r.json()
+          ScreenValues = json2html.convert(json = inp)
+          ScreenValues = HttpResponse(json2html.convert(json = inp))
+    #return HttpResponse("wercker Account details are %s." % question_id)
+
+    return HttpResponse("Wercker Account Details are %s." % ScreenValues)
+
